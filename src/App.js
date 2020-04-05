e320b36630e95a3494725d1b283f70d0b1ac321d
Phil Sun <psun.04@gmail.com>
2020-04-05 11:26:40 -0500

    create header, balance, and income expense components

--- src/App.js
+++ src/App.js
@@ -1,8 +1,20 @@
 import React from 'react';
+import { Header } from './components/Header';
+import { Balance } from './components/Balance';
+import { IncomeExpenses } from './components/IncomeExpenses';
+
 import './App.css';
 
 function App() {
-  return <div>Hello World</div>;
+  return (
+    <div>
+      <Header />
+      <div className='container'>
+        <Balance />
+        <IncomeExpenses />
+      </div>
+    </div>
+  );
 }
 
 export default App;
